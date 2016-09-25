e6b0e3c910d27fc803af817372155984eb370d32
alra <alra@alradeMacBook-Pro.local>
2016-09-25 10:30:50 +0800

    weibo

--- focus/weibo_views.py
+++ focus/weibo_views.py
@@ -9,7 +9,7 @@ from .models import Topic, Article, Comment, Poll, MyUser, Status, Statistics, W
 # from django.http import JsonResponse
 # from django.views.decorators.cache import cache_page
 # from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# from cms.settings import ARTICLE_NUM_ONE_PAGE, COMMENT_NUM_ONE_PAGE
+from cms.settings import APP_KEY, APP_SERCET, CALLBACK_URL
 
 from django.http import (
     Http404, HttpResponse, HttpResponsePermanentRedirect, HttpResponseRedirect,
@@ -17,32 +17,37 @@ from django.http import (
 
 import cms.settings
 import datetime
-from weibo import APIClient
+
+from weibo import Client as WeiboClient
 
 
 def weibo_login(request):
-    """微博登录"""
-    client = APIClient(app_key=cms.settings.APP_KEY, app_secret=cms.settings.APP_SERCET,
-                       redirect_uri=cms.settings.CALLBACK_URL)
-    url = client.get_authorize_url()
-    return HttpResponseRedirect(url)
+    c = WeiboClient(APP_KEY, APP_SERCET, CALLBACK_URL)
+    print c.authorize_url
+    return HttpResponseRedirect(c.authorize_url)
 
 
 def weibo_check(request):
     code = request.GET.get('code', None)
     now = datetime.datetime.now()
     if code:
-        client = APIClient(app_key=cms.settings.APP_KEY, app_secret=cms.settings.APP_SERCET,
-                           redirect_uri=cms.settings.CALLBACK_URL)
-        r = client.request_access_token(code)
-        access_token = r.access_token   # 返回的token，类似abc123xyz456
-        expires_in = r.expires_in       # token过期的UNIX时间：http://zh.wikipedia.org/wiki/UNIX%E6%97%B6%E9%97%B4
-        uid = r.uid
+        c = WeiboClient(APP_KEY, APP_SERCET, CALLBACK_URL)
+        c.set_code(code)
+
+        c.get('users/show', uid=c.uid)
+        uid = c.uid
+        access_token = c.access_token
+
+        # r = client.request_access_token(code)
+        # access_token = r.access_token   # 返回的token，类似abc123xyz456
+        # expires_in = r.expires_in       # token过期的UNIX时间：http://zh.wikipedia.org/wiki/UNIX%E6%97%B6%E9%97%B4
+        # uid = r.uid
         # 在此可保存access token
-        client.set_access_token(access_token, expires_in)
-        request.session['access_token'] = access_token
-        request.session['expires_in'] = expires_in
-        request.session['uid'] = uid
+        # client.set_access_token(access_token, expires_in)
+        # request.session['access_token'] = access_token
+        # request.session['expires_in'] = expires_in
+        # request.session['uid'] = uid
+
         user = WeiboUser(access_token=access_token, uid=uid, request=request)      # 实例化超级微博类
         # 更新数据库
         my_user = MyUser.objects.filter(email=user.get_email())
